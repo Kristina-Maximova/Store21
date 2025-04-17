266b7ac5f64d8409dd5f50d520a2c76e81153026
Kristina Maximova <maximovaki@gmail.com>
2025-04-17 23:21:13 +0300

    registered Category & Product in admin.py

--- catalog/models.py
+++ catalog/models.py
@@ -4,8 +4,9 @@ from django.db import models
 # Create your models here.
 
 class Category(models.Model):
+    """ Класс для представления категории"""
     name = models.CharField(max_length=100, verbose_name='наименование')
-    description = models.TextField(null=True, blank=True, verbose_name='описание' )
+    description = models.TextField(null=True, blank=True, verbose_name='описание')
 
     def __str__(self):
         return f"Категория: {self.name}"
@@ -17,11 +18,18 @@ class Category(models.Model):
 
 
 class Product(models.Model):
+    """ Класс для представления продукта"""
     name = models.CharField(max_length=100, verbose_name='наименование')
-    description = models.TextField(null=True, blank=True, )
-    image = models.ImageField(upload_to='photos/', verbose_name='изображение', null=True, blank=True,
+    description = models.TextField(null=True, blank=True)
+    image = models.ImageField(upload_to='photos/',
+                              verbose_name='изображение',
+                              null=True,
+                              blank=True,
                               help_text='Загрузите изображение продукта')
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+    category = models.ForeignKey(Category,
+                                 on_delete=models.SET_NULL,
+                                 null=True, blank=True,
+                                 related_name='products')
     price = models.FloatField(verbose_name='цена в руб.')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
