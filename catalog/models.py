c56c54d17312a403c287a58c8e0b56bfab9f4c08
Kristina Maximova <maximovaki@gmail.com>
2025-04-17 21:27:34 +0300

    add models.py in catalog/

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name='наименование')
+    description = models.TextField(null=True, blank=True, verbose_name='описание' )
+
+    def __str__(self):
+        return f"Категория: {self.name}"
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        # db_table = ''
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name='наименование')
+    description = models.TextField(null=True, blank=True, )
+    image = models.ImageField(upload_to='photos/', verbose_name='изображение', null=True, blank=True,
+                              help_text='Загрузите изображение продукта')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+    price = models.FloatField(verbose_name='цена в руб.')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.name}: {self.price}руб."
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+        ordering = ['price']
+        # db_table = ''
