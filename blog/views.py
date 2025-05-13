3e502c35f763c4f329f7620385460486b8de674b
Kristina Maximova <maximovaki@gmail.com>
2025-05-14 00:20:58 +0300

    add style for Booleanfield in forms.py

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,7 @@
+from django.core.mail import send_mail
 from django.urls import reverse, reverse_lazy
 from django.views.generic import DetailView, ListView
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
-
-from django.core.mail import send_mail
 
 from .forms import PostForm
 from .models import Post
--- catalog/forms.py
+++ catalog/forms.py
@@ -1,16 +1,22 @@
+import re
+
 from django import forms
 from django.core.exceptions import ValidationError
+from django.forms import BooleanField
+
+from config.settings import SPAM_LIST
 
 from .models import Product
-from config.settings import SPAM_LIST
-import re
 
 
 class StyleFormMixin:
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for field_name, field in self.fields.items():  # в self.fields() получим словарь: {название поля:значение}
-            field.widget.attrs['class'] = "form-control"
+            if isinstance(field, BooleanField):
+                field.widget.attrs['class'] = "form-check-input"
+            else:
+                field.widget.attrs['class'] = "form-control"
 
 
 class ProductForm(StyleFormMixin, forms.ModelForm):
