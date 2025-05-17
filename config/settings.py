0dccf712844c49b7b68a0ae9c1065616fd391082
Kristina Maximova <maximovaki@gmail.com>
2025-05-17 20:27:13 +0300

    small edits in settings.py

--- config/settings.py
+++ config/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 import os
 from pathlib import Path
 
-from django.conf.global_settings import MEDIA_URL
+from django.conf.global_settings import MEDIA_URL, LOGIN_REDIRECT_URL
 from dotenv import load_dotenv
 
 # Загрузка переменных из .env-файла
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if os.getenv('DEBUG') == "True" else False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
@@ -146,5 +146,13 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')  # sender's email-id
 EMAIL_HOST_PASSWORD = os.getenv(
     'EMAIL_HOST_PASSWORD')  # 'app_password' associated with above email-id (not the regular password)
-DEFAULT_FROM_EMAIL = 'maximovaki@gmail.com'
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 DEFAULT_TO_EMAIL = 'Your email'
+
+LOGIN_REDIRECT_URL = '' # куда перенаправлять после успешного входа с паролем
+
+
+# для метода валидации в форме продукта
+SPAM_LIST = ['казино', 'криптовалюта', 'крипта',
+             'биржа', 'дешево', 'бесплатно',
+             'обман', 'полиция', 'радар']
